6a4677ec00de3f8413a94fde34c2d8c74507b94c
Eduardo do Prado <dudsprado999@gmail.com>
2019-09-19 11:26:46 -0300

    c#

---
+++ CalculaDORA/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            double total;
+            int oper;
+
+            Console.WriteLine("Digite o 1º Número:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 2º Número");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o operador desejado?");
+            Console.WriteLine("(1) Soma | (2) Multiplicação | (3) Divisão | (4) Subtração ");
+            oper = int.Parse(Console.ReadLine());
+            
+            switch (oper) {
+                case 1:
+                total = num1 + num2;
+                Console.WriteLine("O valor dessa operação é: {0}",total);
+                break;
+                case 2:
+                total = num1 * num2;
+                Console.WriteLine("O valor dessa operação é: {0}",total);
+                break;
+                case 3:
+                total = num1 / num2;
+                Console.WriteLine("O valor dessa operação é: {0}",total);
+                break;
+                case 4:
+                total = num1 - num2;
+                Console.WriteLine("O valor dessa operação é: {0}",total);
+                break;
+                default:
+                Console.WriteLine("Operação inválida");
+                break;
+            }
+
+        
+        }
+    }
+}
---
+++ Idade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            string oper;
+
+            Console.WriteLine ("Digite sua idade");
+            num1 = int.Parse(Console.ReadLine());
+
+            oper = Console.ReadLine(); 
+
+            Console.WriteLine ($"{num1} * {12} = {num1 * 12}");
+            Console.WriteLine ($"{num1} * {365} = {num1 * 365}");
+            Console.WriteLine ($"{num1} * {8760} = {num1 * 8760}");
+            Console.WriteLine ($"{num1} * {525600} = {num1 * 525600}");
+        }
+    }
+}
+
