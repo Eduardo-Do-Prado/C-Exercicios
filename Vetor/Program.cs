6fb103cec7d7496b610f3541e615831a39eee488
Eduardo do Prado <dudsprado999@gmail.com>
2019-09-25 11:26:57 -0300

    dsa

---
+++ Vetor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int[] vetor = new int[6];
+        int x;
+
+        for(x =0, x<=)
+
+        if (x % 2 == 0){
+                Console.WriteLine("Par");
+
+            }else{
+                Console.WriteLine("Impar");
+                }
+        }
+    }
+}
