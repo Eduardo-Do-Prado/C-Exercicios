015fbbbcd8e899f3a12544000a428453daddbc3a
Eduardo do Prado <dudsprado999@gmail.com>
2019-09-26 11:21:38 -0300

    seu jorge

--- Vetor/Program.cs
+++ Vetor/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 
-namespace Vetor
+namespace Vetor_numérico
 {
     class Program
     {
         static void Main(string[] args)
         {
-        int[] vetor = new int[6];
-        int x;
+            int[] vetor = new int[6];
+            int pares = 0;
+            int impares = 0;
 
-        for(x =0, x<=)
+            for(int cont = 0; cont <= 5; cont++){
+                Console.Write("Digite um número: ");
+                vetor[cont] = int.Parse(Console.ReadLine());
 
-        if (x % 2 == 0){
-                Console.WriteLine("Par");
+            }
 
-            }else{
-                Console.WriteLine("Impar");
+            foreach (int num in vetor)
+            {
+                if(num%2 == 0){
+                    //pares = pares + 1;
+                    //pares += 1;
+                    pares += 1;
+                } else {
+                    impares++;
                 }
+            }
+
+            Console.WriteLine($"Você tem {pares} números pares e {impares} números mpares");
         }
     }
 }
